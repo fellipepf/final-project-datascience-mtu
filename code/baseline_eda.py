e1c5ebe28f7e43621b521544caef9d1fd8936a4d
fellipeferreira <fellipepf@gmail.com>
2021-12-11 23:40:47 +0000

    create graphs for one day and time period

--- code/baseline_eda.py
+++ code/baseline_eda.py
@@ -6,9 +6,10 @@ import sys
 import numpy as np
 import pandas as pd
 import glob
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt, dates as mdates
 import seaborn as sns
-
+import matplotlib.ticker as ticker
+from matplotlib.pyplot import figure
 
 class ExportBaseline:
     def __init__(self):
@@ -72,7 +73,7 @@ def remove_outliers(baseline, y_feature):
     q75 = baseline[y_feature].quantile(0.75)
     intr_qr = q75 - q25
 
-    print('Percentiles: 25th=%.3f, 75th=%.3f, IQR=%.3f' % (q25, q75, intr_qr))
+    #print('Percentiles: 25th=%.3f, 75th=%.3f, IQR=%.3f' % (q25, q75, intr_qr))
 
     max = q75 + (1.5 * intr_qr)
     min = q25 - (1.5 * intr_qr)
@@ -90,6 +91,108 @@ def check_nullvalues(baseline):
     #check for null values
     print(baseline.isnull().sum())
 
+
+#
+def graph_timeserie(patient):
+    # graph
+
+    userid = 'patient_1'
+    df_patient = patient[userid]['timeserie']
+
+    df_patient.plot(x="timestamp",y="activity")
+    plt.show()
+
+
+
+##### Baseline period of the day EDA graphs
+def baseline_dimension(baseline_time_period):
+    shape = baseline_time_period.shape
+    print(f'Lines: {shape[0]} - columns: {shape[1]}')
+
+def box_plot_simple(baseline_time_period, list_columns):
+    #baseline_eda.eda_baseline_boxplot_remove_outliers(baseline_time_period, list, True)
+    baseline_time_period.boxplot(column=list_columns)
+
+    plt.figure(figsize=(20, 6))
+    plt.show()
+
+
+def plot_line_graph(df_time_period):
+    id = "control_31"
+    values = df_time_period[id]
+
+    list_df_morning = values['morning']
+    merged = pd.concat(list_df_morning)
+
+    sns.set(rc={'figure.figsize': (11, 4)})
+    merged['activity'].plot(linewidth=0.5);
+    plt.show()
+
+
+def plot_graph_one_day(df_time_period, id_user=None):
+    id_user = "control_31"
+    values = df_time_period[id_user]
+
+    df_first_day = values['timeserie'][0]
+
+
+    sns.set(rc={'figure.figsize': (11, 4)})
+    sns.set_style("ticks")
+    plot = sns.lineplot(x="datetime", y="activity", data=df_first_day)
+    plot.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y %H:%M'))
+    #plot.axvline("%d-%m-%Y 06:00", color="red", linestyle="dashed")
+    plt.title(f"Time Serie of user: {id_user}")
+    plt.ylabel("Activity")
+    plt.xticks(rotation=25)
+    plt.show()
+
+
+
+def plot_graph_periods_of_day(df_time_period, id_user=None):
+    id_user = "control_1"
+
+    values = df_time_period[id_user]
+
+    df_first_day = values['timeserie'][0]
+    df_first_day = df_first_day.set_index('datetime')
+    # from midnight to midday
+    morning = df_first_day.between_time('0:00', '6:59')
+
+    # this is one day range so after 23:59 is not in this period of time
+    afternoon = df_first_day.between_time('7:00', '19:00', include_start=True, include_end=False)
+    evening = df_first_day.between_time('19:00', '00:00', include_start=True, include_end=False)
+
+    fig, ax = plt.subplots(3, 1, figsize=(11, 4))
+
+    plot = sns.lineplot(x="datetime", y="activity",  data=morning, ax=ax[0])
+    plot.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    #plot.xaxis.set_major_locator(ticker.LinearLocator(10))
+    #ax[0].tick_params(labelrotation=25)
+    #ax[0].set_title('Morning')
+    ax[0].set_xlabel("Time")
+    ax[0].set_ylabel("Activity")
+
+
+
+    plot = sns.lineplot(x="datetime", y="activity", data=afternoon, ax=ax[1])
+    plot.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    #ax[1].tick_params(labelrotation=25)
+    #ax[1].set_title('Afternoon')
+    ax[1].set_xlabel("Time")
+    ax[1].set_ylabel("Activity")
+
+
+    plot = sns.lineplot(x="datetime", y="activity",  data=evening, ax=ax[2])
+    plot.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    #ax[2].tick_params(labelrotation=25)
+    #ax[2].set_title('Evening')
+    ax[2].set_xlabel("Time")
+    ax[2].set_ylabel("Activity")
+
+
+    fig.suptitle(f"Time Serie of user: {id_user}")
+
+    plt.show()
 
 if __name__ == '__main__':
 
