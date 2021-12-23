f7207ec11bbaf9de3704480dc295c2a7a38b136f
fellipeferreira <fellipepf@gmail.com>
2021-12-23 19:04:14 +0000

    Deep learning model working fine. Need refactoring on the code.

--- code/lstm_model.py
+++ code/lstm_model.py
@@ -18,15 +18,18 @@ from keras.layers import Flatten
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling1D
 #from keras.utils import to_categorical
 from keras.utils.np_utils import to_categorical
 from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping
 
 from datetime import datetime
 
 from tensorflow import keras
 
+import log_configuration
 from psykose import LoadDataset, PreProcessing, eda_baseline_date_range
 
 
@@ -73,23 +76,27 @@ def process_dataset(dataset):
     return x_values, y_values
 
 def create_time_window(df_dataset):
-    N_TIME_STEPS = 300  # 50 records in each sequence
-    N_FEATURES = 1  # activuty
-    step = 10  # window overlap = 50 -10 = 40  (80% overlap)
+    logger = log_configuration.logger
+    logger.info("Create time window...")
 
+    window_size = 300  # 50 records in each sequence
+    N_FEATURES = 2  # activity and category
+    step = 10  # window overlap = 50 -10 = 40  (80% overlap)
 
     segments = []
     labels = []
     classes = [c.name for c in df_dataset['class']]
-    for i in range(0, df_dataset.shape[0] - N_TIME_STEPS, step):
-        xs = df_dataset['activity'].values[i: i + N_TIME_STEPS]
+    for i in range(0, df_dataset.shape[0] - window_size, step):
+        x_activity = df_dataset['activity'].values[i: i + window_size]
+        x_category = df_dataset['category'].values[i: i + window_size]
 
-        label = stats.mode(classes[i: i + N_TIME_STEPS])[0][0]
-        segments.append([xs])
-        labels.append(label)
+        y_label = stats.mode(classes[i: i + window_size])[0][0]
+
+        segments.append([x_activity, x_category])
+        labels.append(y_label)
 
     # reshape the segments which is (list of arrays) to one list
-    reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, N_TIME_STEPS, N_FEATURES)
+    reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, window_size, N_FEATURES)
     labels = np.asarray(pd.get_dummies(labels), dtype=np.float32)
     reshaped_segments.shape
     print(labels)
@@ -99,6 +106,9 @@ def create_time_window(df_dataset):
 
 
 def split_dataset(x_values, y_values):
+    logger = log_configuration.logger
+    logger.info("Splitting dataset...")
+
     RANDOM_SEED = 24
     #x = dstack(x_values)
     X_train, X_test, y_train, y_test = train_test_split(x_values, y_values, test_size=0.2, random_state=RANDOM_SEED)
@@ -139,15 +149,51 @@ def evaluate_ltsm_model(X_train, X_test, y_train, y_test):
 
 
 def evaluate_cnn_model(X_train, X_test, y_train, y_test):
-    print("CNN model running...")
+    logger = log_configuration.logger
+    logger.info("CNN model running...")
+
     verbose = 1
     epochs = 20
     batch_size = 32
 
-    n_timesteps, n_features, n_outputs = X_train.shape[1], X_train.shape[2], y_train.shape[1]
+    sample =  X_train.shape[0]
+    n_timesteps = X_train.shape[1]
+    n_features = X_train.shape[2]
+    n_outputs =  y_train.shape[1]
 
     model = Sequential()
-    model.add(Conv1D(filters=128, kernel_size=3, activation='relu', input_shape=(n_timesteps, n_features)))
+    model.add(Conv2D(filters=128, kernel_size=4, activation='relu', input_shape=(sample, n_timesteps, n_features)))
+    model.add(Conv2D(filters=128, kernel_size=4, activation='relu'))
+    model.add(Conv2D(filters=128, kernel_size=4, activation='relu'))
+    model.add(Dropout(0.5))
+    #model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(n_outputs, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    # fit network
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    # evaluate model
+    _, accuracy = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=verbose)
+    return accuracy
+
+
+def evaluate_cnn_model_working(X_train, X_test, y_train, y_test):
+    logger = log_configuration.logger
+    logger.info("CNN model running...")
+
+    verbose = 1
+    epochs = 20
+    batch_size = 32
+
+    #sample =  X_train.shape[0]
+    n_timesteps = X_train.shape[1]
+    n_features = X_train.shape[2]
+    n_outputs =  y_train.shape[1]
+
+    model = Sequential()
+    model.add(Conv1D(filters=128, kernel_size=3, activation='relu', input_shape=( n_timesteps, n_features)))
     model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
     model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
     model.add(Dropout(0.5))
@@ -156,6 +202,7 @@ def evaluate_cnn_model(X_train, X_test, y_train, y_test):
     model.add(Dense(100, activation='relu'))
     model.add(Dense(n_outputs, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
     # fit network
     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
     # evaluate model
@@ -163,12 +210,34 @@ def evaluate_cnn_model(X_train, X_test, y_train, y_test):
     return accuracy
 
 
+def check_options(*options):
+    '''
+    Verify if only one validation/split method is chosen
+    If more than one the script stop running
+    :param options:
+    :return:
+    '''
+    logger = log_configuration.logger
+
+    try:
+        assert sum(options) == 1
+    except Exception as e:
+        logger.error("Only one option must be chosen")
+        exit()
+
 if __name__ == '__main__':
+    logger = log_configuration.logger
+    logger.info("Script started...")
 
     dict_dataset = LoadDataset().get_dataset_joined()
-
     preProcessing = PreProcessing(dict_dataset)
-    df_dataset = preProcessing.df_dataset
+    df_dataset = preProcessing.df_dataset_category
+
+    use_k_fold_cross_validation = False
+    use_leave_one_out = True
+    use_traditional_split = True
+
+    check_options(use_k_fold_cross_validation, use_leave_one_out, use_traditional_split)
 
     x_values, y_values = create_time_window(df_dataset)
 
@@ -177,8 +246,10 @@ if __name__ == '__main__':
 
     start_time = datetime.now()
 
-    evaluate_ltsm_model(X_train, X_test, y_train, y_test)
+    #evaluate_ltsm_model(X_train, X_test, y_train, y_test)
     #evaluate_cnn_model(X_train, X_test, y_train, y_test)
+
+    evaluate_cnn_model_working(X_train, X_test, y_train, y_test)
 
     time_elapsed = datetime.now() - start_time
     print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
